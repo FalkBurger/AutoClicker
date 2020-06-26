602ed08b3138f3cf81a6d880fd40ff03e654cc7e
Falk Burger <falkburger1@gmail.com>
2020-06-26 16:43:35 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace AutoClicker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            while (Console.ReadKey().Key != ConsoleKey.Escape)
+            {
+                if (Console.ReadKey().Key == ConsoleKey.L)
+                {
+                    Console.WriteLine("Do the thing!");
+                    MouseOperations OpHandle = new MouseOperations();
+                    MouseOperations.MouseEventFlags GoDown = MouseOperations.MouseEventFlags.LeftDown;
+                    MouseOperations.MouseEvent(GoDown);
+                    MouseOperations.MouseEventFlags AndUp = MouseOperations.MouseEventFlags.LeftUp;
+                    MouseOperations.MouseEvent(AndUp);
+                }
+            }
+        }
+    }
+}
